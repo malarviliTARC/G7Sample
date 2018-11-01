accfa86d55eabc29a02b276005b99f516d7e0e6b
Tarc <Tarc@B87>
2018-11-01 10:59:37 +0800

    update branch sample

--- src/g7/G7.java
+++ src/g7/G7.java
@@ -16,7 +16,8 @@ public class G7 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world 1");
-        System.out.println("Hello world 1");
+        System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
     }
     
 }
